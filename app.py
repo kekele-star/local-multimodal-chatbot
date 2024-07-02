7c57347ad75c8a6c273109c08e7cdce970ea707b
Kekele <esinamdemanya@gmail.com>
2024-07-02 09:21:51 +0000

    basic chat implementation

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+def main():
+    st.title("Multimodla Local Chat App")
+    chat_container = st.containier()
+
+    user_input= st.text_input("Type your message here", key="user_input")
+
+    send_button = st.button("Send", key="send_button")
+
+    if send_button:
+        llm_response = "This is a response from the LLM model"
+
+    with chat_container:
+        st.chat_message("user").write(user_input)
+        st.chat_message("ai").write("here is an answer")
+
+if __name__ == "__main__":
+    main()
